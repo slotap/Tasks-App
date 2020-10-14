d8c7ea50f51c34d9969144d33871531a96fbe404
Piotr Slota <slotap@gmail.com>
2020-10-14 12:32:34 +0200

    15.2 Task - added findTaskById

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -5,6 +5,7 @@ import com.crud.tasks.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -17,5 +18,9 @@ public class DbService {
 
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public Task findTaskById(Long id){
+        return taskRepository.findTaskById(id);
     }
 }
