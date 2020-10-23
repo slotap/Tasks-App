0957fd3d8fa8fcaf72d7b17703b77d92acca5a2d
Piotr Slota <slotap@gmail.com>
2020-10-23 21:57:24 +0200

    18. Added showtasks.bat

---
+++ showtasks.bat
@@ -0,0 +1,12 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto startupbrowser
+echo.
+echo failed to run runcrud.bat
+goto fail
+
+:startupbrowser
+start chrome http://localhost:8080/crud/v1/task/getTasks
+
+:fail
+echo.
+echo There were errors
