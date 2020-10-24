e561d136e38f6a6705cf75a91420c78c223c6b65
Piotr Slota <slotap@gmail.com>
2020-10-24 14:06:46 +0200

    14.4 Controller Method Mappings

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,8 +1,7 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,18 +9,28 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+
+    @GetMapping(value = "getTasks")
     public List<TaskDto> getTasks(){
         return new ArrayList<>();
     }
-    public TaskDto getTask(Long taskId){
+
+    @GetMapping(value = "getTask/{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId){
         return new TaskDto((long)1,"Test Task","Test Task content");
     }
-    public void deleteTask(Long taskId){
+
+    @DeleteMapping(value = "deleteTask/{taskId}")
+    public void deleteTask(@PathVariable Long taskId){
 
     }
+
+    @PutMapping(value = "updateTask")
     public TaskDto updateTask(TaskDto task){
         return new TaskDto(1L,"Updated task","Updated task content");
     }
+
+    @PostMapping(value = "createTask")
     public void createTask(TaskDto taskDto){
 
     }
