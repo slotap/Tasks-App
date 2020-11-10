af20d87882dc4873aecd22fb6694133c3fe8914e
Piotr Slota <slotap@gmail.com>
2020-11-10 10:40:55 +0100

    Added EmailScheduler

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 
 import javax.validation.constraints.NotNull;
 
-@Builder
+@Builder(access = AccessLevel.PUBLIC)
 @Getter
 public class Mail {
     @NotNull
---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,37 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.CreatedTrelloCard;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TrelloService {
+    private static final String SUBJECT = "Tasks : Create Trello Card" ;
+    private final SimpleEmailService emailService;
+    private final TrelloClient trelloClient;
+    private final AdminConfig adminConfig;
+
+    public List<TrelloBoardDto> fetchTrelloBoards() {
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+        Optional.ofNullable(newCard).ifPresent(card -> emailService.send(Mail.builder()
+                .mailTo(adminConfig.getAdminMail())
+                .subject(SUBJECT)
+                .message("New card: " + trelloCardDto.getName() + " has been created on your Trello account")
+                .build()
+        ));
+        return newCard;
+    }
+}
