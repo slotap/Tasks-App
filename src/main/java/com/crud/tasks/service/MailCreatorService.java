6c46839e0e77edf190700b338e23f672f28d7b50
Piotr Slota <slotap@gmail.com>
2020-12-14 08:40:36 +0100

    Thymeleaf - added personalized email greetings and list of application functionalities

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
 
@@ -24,14 +27,23 @@ public class MailCreatorService {
     private CompanyConfig companyConfig;
 
     public String builtTrelloCardEmail(String message){
+
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/task_frontend/");
         context.setVariable("button", "Visit website");
-        context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", GOODBYE_MESSAGE);
         context.setVariable("company_name", companyConfig.getCompanyName());
         context.setVariable("company_email", companyConfig.getCompanyEmail());
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
         return templateEngine.process("templates.mail/created-trello-card-mail", context);
     }
 }
