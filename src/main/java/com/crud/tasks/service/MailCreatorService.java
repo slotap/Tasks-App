8fa72c6574908940c4b19266c69cf8340bd415d4
Piotr Slota <slotap@gmail.com>
2021-01-07 13:47:10 +0100

    Thymeleaf - added goodbye message and company info to email template

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.thymeleaf.context.Context;
 
 @Service
 public class MailCreatorService {
+
+    private static final String GOODBYE_MESSAGE = "Kind Regards";
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -16,12 +20,18 @@ public class MailCreatorService {
     @Autowired
     private AdminConfig adminConfig;
 
+    @Autowired
+    private CompanyConfig companyConfig;
+
     public String builtTrelloCardEmail(String message){
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("tasks_url", "http://localhost:8888/task_frontend/");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", GOODBYE_MESSAGE);
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_email", companyConfig.getCompanyEmail());
         return templateEngine.process("templates.mail/created-trello-card-mail", context);
     }
 }
